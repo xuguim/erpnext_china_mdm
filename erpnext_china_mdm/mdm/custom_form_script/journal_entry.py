65222d4f87e6350357d095cb85fb9087ae551c85
cxy7a7b7c <3327083372@qq.com>
2024-11-23 10:04:58 +0800

    feat: journal entry

---
+++ erpnext_china_mdm/mdm/custom_form_script/journal_entry.py
@@ -0,0 +1,14 @@
+import frappe
+
+from erpnext.accounts.doctype.journal_entry.journal_entry import JournalEntry
+
+class CustomJournalEntry(JournalEntry):
+
+    def before_save(self):
+        name = frappe.db.exists("Payment Entry", {"custom_bank_serial_number": self.custom_bank_serial_number})
+        if name:
+            docstatus = frappe.db.get_value('Payment Entry', name, 'docstatus')
+            if docstatus == 0:
+                frappe.db.delete('Payment Entry', name)
+            elif docstatus == 1:
+                frappe.throw('无法创建，存在相同银行流水号的收付款凭证！')
