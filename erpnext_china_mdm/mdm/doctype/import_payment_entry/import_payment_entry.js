df0676bcbbf4bc0b3daec8a986daafb3d61a9d11
ilqc <iliuchao2326@163.com>
2024-12-03 18:21:38 +0800

    feat: import payment entry

--- erpnext_china_mdm/mdm/doctype/import_payment_entry/import_payment_entry.js
+++ erpnext_china_mdm/mdm/doctype/import_payment_entry/import_payment_entry.js
@@ -2,37 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Import Payment Entry", {
-    // bank(frm){
-    //     frm.call('get_bank_account').then((r)=>{
-    //         const account = r.message.account;
-    //         console.log(account)
-    //         if (account) {
-    //             frm.doc.bank_account = account;
-    //             frm.refresh_field('bank_account');
-    //         }
-    //     });
-    // },
+    setup: function (frm) {
+		frm.set_query("party_type", function () {
+            let party_types = Object.keys(frappe.boot.party_account_types);
+			return {
+				filters: {
+					name: ["in", party_types],
+				},
+			};
+		});
+	},
 	refresh(frm) {
-        frm.add_custom_button(__('开始导入'), () => {
-            frm.call('import_payment_entry').then((r)=>{
+        // frm.add_custom_button(__('开始导入'), () => {
+        //     frm.call('import_payment_entry').then((r)=>{
 
-            })
-        });
+        //     })
+        // });
         frm.set_query("bank_account", function(doc) {
             return {
                 filters: [
                     ["Bank Account", "is_company_account", "=", 1],
                     ["Bank Account", "company", "=", doc.company],
-                    // ["Bank Account", "bank", "=", doc.bank]
+                    ["Bank Account", "bank", "=", doc.bank]
                 ]
             };
         });
-        frm.call('get_temporary_customer').then((r)=>{
-            const customer = r.message.customer;
-            if (customer) {
-                frm.doc.temporary_customer = customer;
-                frm.refresh_field('temporary_customer');
-            }
-        })
 	},
 });
