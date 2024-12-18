131aed3694edce9e097c7e2305ad8d0e70604f6f
ilqc <iliuchao2326@163.com>
2024-12-18 11:54:36 +0800

    Revert "feat: perssmion customer"
    This reverts commit 09c58c7f56cb8682c1063da6cf1adb7ca8ca893c.

--- erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
+++ erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
@@ -16,14 +16,10 @@ def has_query_permission(user):
 		# 这里要找 已转化的线索
 		leads = frappe.db.get_all("Lead", filters=[["lead_owner", '=', user]], pluck='name')
 		leads_str = str(tuple(leads)).replace(',)',')')
-		
-        # 如果客户是 内部客户，也可以看到
-		internal_customers = frappe.db.get_all("Customer", filters={"is_internal_customer": 1}, pluck='name')
-		internal_customers_str = str(tuple(internal_customers)).replace(',)',')')
-		conditions = f"owner in {users_str} or lead_name in {leads_str} or name in {internal_customers_str}" 
+		conditions = f"owner in {users_str} or lead_name in {leads_str}" 
 	return conditions
 
-def has_permission(doc, ptype, user, permission_type=None):
+def has_permission(doc, user, permission_type=None):
 	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager']]}):
 		# 如果角色包含管理员，则看到全量
 		return True
@@ -34,10 +30,6 @@ def has_permission(doc, ptype, user, permission_type=None):
 
 		# 如果当前客户的线索的线索负责人是当前user，也可以看到
 		lead_owner = frappe.db.get_value("Lead", doc.lead_name, fieldname="lead_owner")
-
-        # 如果当前客户是内部客户，并且当前用户包含销售权限，则给只读权限
-		if doc.is_internal_customer and ptype == 'read' and frappe.db.get_value('Has Role',{'parent':user,'role':['in',['销售']]}):
-			return True
 
 		if doc.owner in users or lead_owner == user:
 			return True
