fcf86ecc26b5e95e2fc8d284ded29f2c4deb219a
allen.xu <xu.gui.m@gmail.com>
2024-12-29 21:00:32 +0800

    feat: Customize Stock User's delivery note permission #92

--- erpnext_china_mdm/mdm/custom_permission/address/permission_address.py
+++ erpnext_china_mdm/mdm/custom_permission/address/permission_address.py
@@ -1,6 +1,6 @@
 import frappe
-
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
+from erpnext_china_mdm.mdm.custom_permission.customer.permission_customer import get_customer_from_delivery_note
 
 def get_addresses_from_customers(user):
 	try:
@@ -10,6 +10,21 @@ def get_addresses_from_customers(user):
 				'link_doctype': 'Customer', 
 				'parenttype': 'Address',
 				'link_name': ['in', customers]},
+				pluck='parent')
+			is_your_company_addresses = frappe.get_all('Address', filters={'is_your_company_address': 1}, pluck='name')
+			return list(set(addresses + is_your_company_addresses))
+		return []
+	except:
+		return []
+
+def get_addresses_from_delivery_notes(user):
+	try:
+		if 'Stock User' in frappe.get_roles(user):
+			dn_customers = get_customer_from_delivery_note(user)
+			addresses = frappe.get_all('Dynamic Link', filters={
+				'link_doctype': 'Customer', 
+				'parenttype': 'Address',
+				'link_name': ['in', dn_customers]},
 				pluck='parent')
 			is_your_company_addresses = frappe.get_all('Address', filters={'is_your_company_address': 1}, pluck='name')
 			return list(set(addresses + is_your_company_addresses))
@@ -27,7 +42,11 @@ def has_query_permission(user):
 		users.append(user)
 		users_str = str(tuple(users)).replace(',)',')')
 		conditions = f"tabAddress.`owner` in {users_str}"
-
+		addresses_from_delivery_notes = []
+		if 'Stock User' in frappe.get_roles(user):
+			addresses_from_delivery_notes = get_addresses_from_delivery_notes(user)
+			addresses_str = str(tuple(addresses_from_delivery_notes)).replace(',)',')')
+			conditions += f" or tabAddress.`name` in {addresses_str}"
 		# # 有客户权限，则有详细地址权限
 		# addresses = get_addresses_from_customers(user)
 		# if len(addresses) > 0:
@@ -44,11 +63,14 @@ def has_permission(doc, user, permission_type=None):
 		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
 		users.append(user)
+		addresses_from_delivery_notes = []
+		if 'Stock User' in frappe.get_roles(user):
+			addresses_from_delivery_notes = get_addresses_from_delivery_notes(user)
 
 		# 有客户权限，则有详细地址权限
 		# addresses = get_addresses_from_customers(user)
 
-		if doc.owner in users or doc.name:
+		if doc.owner in users or doc.name or doc.name in addresses_from_delivery_notes:
 			return True
 		else:
 			return False
