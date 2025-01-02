69e896ba6ee46f443b2ab5fa4f139f1c550e65e1
digitwise <407469895@qq.com>
2025-01-02 15:03:21 +0800

    Update permission_customer.py

--- erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
+++ erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
@@ -2,6 +2,25 @@ import frappe
 
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
 
+def get_descendants(name, nodes):
+    descendants = []
+    for node in nodes:
+        if node['parent_warehouse'] == name:
+            descendants.append(node)
+            descendants.extend(get_descendants(node['name'], nodes))
+    return descendants
+
+def get_user_all_warehouses(users):
+	user_warehouses = frappe.get_all("Warehouse User", filters={"warehouse_user": ["in", users]}, pluck='parent')
+	warehouses = frappe.get_all("Warehouse", filters={
+		"name": ["in", list(set(user_warehouses))]
+	}, fields=["name", "parent_warehouse"])
+	all_warehouses = frappe.get_all("Warehouse", fields=["name", "parent_warehouse"])
+	finall_warehouses = []
+	for w in warehouses:
+		finall_warehouses += get_descendants(w['name'], all_warehouses) + [w]
+	return finall_warehouses
+
 def get_customers_from_sales_orders(user):
 	try:
 		customers = []
