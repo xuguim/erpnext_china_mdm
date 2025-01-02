6419171652180d5cf6e892f697dded33d896f23c
digitwise <407469895@qq.com>
2025-01-02 16:41:55 +0800

    Update permission_customer.py

--- erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
+++ erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
@@ -106,7 +106,7 @@ def has_permission(doc, user, permission_type=None):
 
 		# 仓库、发货
 		if '仓库' in frappe.get_roles(user):
-			delivery_note_docs = frappe.get_all('Delivery Note', filters={"customer": doc.name,'workflow_state':'仓库审核','Approved'}, pluck='name')
+			delivery_note_docs = frappe.get_all('Delivery Note', filters={"customer": doc.name,'workflow_state':['in',['仓库审核','Approved']]}, pluck='name')
 			delivery_note_warehouses = frappe.get_all("Delivery Note Item", filters={
 						"parent": ["in", delivery_note_docs],
 						"parenttype": "Delivery Note",
@@ -117,7 +117,7 @@ def has_permission(doc, user, permission_type=None):
 					customer_perm = True
 
 		if 'Delivery User' in frappe.get_roles(user):
-			shippers = frappe.get_all('Delivery Note', filters={"customer": doc.name,'workflow_state':['in',['发货员确认出货','Approved']}, pluck='shipper')
+			shippers = frappe.get_all('Delivery Note', filters={"customer": doc.name,'workflow_state':['in',['发货员确认出货','Approved']]}, pluck='shipper')
 			emp = frappe.get_all('Employee', filters={"user_id":user}, pluck='name')
 			if 'HR-EMP-00828' in shippers or 'HR-EMP-02111' in shippers:
 				shippers = ['HR-EMP-00828','HR-EMP-02111']+shippers
