8b921ec59bb4f1437a4f308a01586ca622a19af2
digitwise <407469895@qq.com>
2025-01-14 16:40:06 +0800

    Update permission_customer.py

--- erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
+++ erpnext_china_mdm/mdm/custom_permission/customer/permission_customer.py
@@ -88,6 +88,7 @@ def has_query_permission(user):
 	return conditions
 
 def has_permission(doc, user, permission_type=None):
+	customer_perm = False
 	if frappe.db.get_value('Has Role',{'parent':user,'role': ['in',['System Manager','销售会计','销售支持']]}):
 		# 如果角色包含管理员，则看到全量
 		return True
