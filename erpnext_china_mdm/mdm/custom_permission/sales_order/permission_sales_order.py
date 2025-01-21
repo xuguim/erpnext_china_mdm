1c32b0630a767eba425f706a0092eadbd54d80d8
digitwise <407469895@qq.com>
2025-01-21 19:37:17 +0800

    Update permission_sales_order.py

--- erpnext_china_mdm/mdm/custom_permission/sales_order/permission_sales_order.py
+++ erpnext_china_mdm/mdm/custom_permission/sales_order/permission_sales_order.py
@@ -3,9 +3,12 @@ import frappe
 from erpnext_china.hrms_china.custom_form_script.employee.employee import get_employee_tree
 
 def has_query_permission(user):
-	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager','销售会计','销售支持']]}):
+	if frappe.db.get_value('Has Role',{'parent':user,'role':['in',['System Manager','财务管理']]}):
 		# 如果角色包含管理员，则看到全量
 		conditions = ''
+	elif frappe.db.get_value('Has Role',{'parent':user,'role':['in',['销售会计','销售支持']]}):
+		# 如果角色包含管理员，则看到全量
+		conditions = f"`custom_original_sales_order` is null"
 	else:
 		# 其他情况则只能看到自己,上级可以看到下级
 		users = get_employee_tree(parent=user)
